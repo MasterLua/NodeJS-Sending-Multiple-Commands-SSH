ca12d3f957ab4565a836bc53f14e9b4cd85983b2
MasterLua (Redouu) <60834623+MasterLua@users.noreply.github.com>
2022-01-19 03:27:42 +0100

    Add files via upload

---
+++ cnc.js
@@ -0,0 +1,37 @@
+var SSH = require('simple-ssh');
+const server = require('./server.json');
+const config = require('./config.json');
+var HasBeenWaiting = false
+const Commands;
+var HasBeenSending = 0
+
+console.log("[+] Thanks to use my script || Created by Redouu.")
+console.log("[+] Starting sending to " + server.length + " servers.")
+console.log("[*] The commands has been sending is : " + Commands + "\n")
+
+if (config.StaticCommands == "Nothing") {
+    Commands = process.argv.slice(2).join(" ");
+}else{
+    Commands = config.StaticCommands;
+}
+
+function SendToArray(ArrayID) {
+    console.log("[-] Sending commands on Server #" + ArrayID + " (" + server[ArrayID].Host + ")");
+    var ssh = new SSH({
+        host: server[ArrayID].Host,
+        port: server[ArrayID].Port,
+        user: server[ArrayID].User,
+        pass: server[ArrayID].Pass
+    });
+    ssh.exec(Commands, {}).start();
+    setTimeout(function(){
+        HasBeenSending++;
+        if(server.length == HasBeenSending) {
+            console.log("\n[!] Sending has been finish. Thanks to use the script.")
+        }else{
+            SendToArray(HasBeenSending);    
+        }
+    }, 1000)    
+}
+
+SendToArray(HasBeenSending);
